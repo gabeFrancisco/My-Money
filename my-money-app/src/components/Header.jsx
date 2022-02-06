cbcf8d8d1cb385fe0c6b6889e356cb77ed90baea
Gabriel Frank <gabegit2@gmail.com>
2022-02-06 15:43:09 -0300

    Started frontend dev and added header component

---
+++ my-money-app/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Header.css";
+
+const Header = (props) => {
+  return (
+    <header className="main-header">
+      <a href="/#/" className="Logo">
+        <i class="far fa-money-bill-alt"></i>
+        <span>My Money</span>
+      </a>
+    </header>
+  );
+};
+
+export default Header;
